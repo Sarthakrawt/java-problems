250f755b0c043870a42bb319fbe1afe1dbc11afa
Sarthakrawt <155871591+Sarthakrawt@users.noreply.github.com>
2024-05-03 23:48:17 +0530

    Add files via upload

---
+++ array-problem/problem.java
@@ -0,0 +1,12 @@
+public class problem {
+    // prints elements which are of even length from an array
+    public static void main(String args[]) {
+        int[] arr = { 23, 344, 54 };
+        for (int a : arr) {
+            int count = String.valueOf(a).length();
+            if (count % 2 == 0) {
+                System.out.println(a);// {23,54}
+            }
+        }
+    }
+}
